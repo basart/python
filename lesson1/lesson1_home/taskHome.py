998384fbfd40871c64ff569c3a41ba7aa4a71851
basart <basov_av@tut.by>
2015-12-03 16:19:47 +0300

    change home task 1

--- lesson1/lesson1_home/taskHome.py
+++ lesson1/lesson1_home/taskHome.py
@@ -1,8 +1,6 @@
-listNumber = [1, 3, 4, 6, 5, 4]
-oddElements = listNumber[1: len(listNumber): 2]
-
-print(oddElements)
-if bool(oddElements):
-    print(reduce(lambda res, x: res*x, oddElements, 1))
+listNumber = [0, 1, 2, 3, 4, 5]
+if listNumber:
+    print(sum(listNumber[1: len(listNumber): 2])*listNumber[-1])
 else:
     print(0)
+
