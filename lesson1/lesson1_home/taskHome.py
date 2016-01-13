6f879ced3f654666e20254df4d786da01fecd7b7
basart <basov_av@tut.by>
2016-01-13 21:44:36 +0300

    change lesson1_home

--- lesson1/lesson1_home/taskHome.py
+++ lesson1/lesson1_home/taskHome.py
@@ -1,6 +1,19 @@
-listNumber = [0, 1, 2, 3, 4, 5]
-if listNumber:
-    print(sum(listNumber[1: len(listNumber): 2])*listNumber[-1])
-else:
-    print(0)
+if __name__ == "__main__":
+    list_number = [0, 1, 2, 3, 4, 5]
+    sum_list_number = 0
+    index_number_in_list = 1
+    try:
+        while index_number_in_list <= len(list_number) - 1:
+            sum_list_number += list_number[index_number_in_list]
+            index_number_in_list += 2
+        last_number = list_number[-1]
+        print(sum_list_number * last_number)
+    except IndexError:
+        print(0)
+
+# print listNumber[1]
+# if listNumber:
+#     print(sum(listNumber[1: len(listNumber): 2])*listNumber[-1])
+# else:
+#     print(0)
 
