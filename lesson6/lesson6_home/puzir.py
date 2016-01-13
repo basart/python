e510c54b8a47c838b2bc5397077a68ba7e245ff0
basart <basov_av@tut.by>
2016-01-13 20:44:33 +0300

    small refactoring lesson6_home

--- lesson6/lesson6_home/puzir.py
+++ lesson6/lesson6_home/puzir.py
@@ -17,39 +17,37 @@ def puz(list_):
             if list_[j] > list_[j+1]:
                 list_[j], list_[j + 1] = list_[j + 1], list_[j]
 
-    return list_
-
-def timer1(test_list):
+def timer_for_sorting_bubble(test_list):
     start = datetime.datetime.now()
-    list_ = []
-    list_ = puz(test_list)
+    puz(test_list)
     finish = datetime.datetime.now()
     return (finish - start).total_seconds()
 
 
-def timer2(test_list):
+def timer_for_quick_sort(test_list):
     start = datetime.datetime.now()
     test_list.sort()
     finish = datetime.datetime.now()
     return (finish - start).total_seconds()
 
-xlist = mlab.frange(10000, 50000, 10000)
-test_list = []
-for i in range(len(xlist)):
-    test_list.append(iter_list(xlist[i]))
 
 
-plt.subplot(211)
-ylist = [timer2(test_list[x/10000-1]) for x in xlist]
-plt.bar(xlist, ylist, 100)
-plt.xticks(xlist)
-plt.yticks(ylist)
+if __name__ == "__main__":
+    xlist = mlab.frange(10000, 50000, 10000)
+    test_list = []
+    for i in range(len(xlist)):
+        test_list.append(iter_list(xlist[i]))
+    plt.subplot(211)
+    ylist = [timer_for_quick_sort(test_list[x/10000-1]) for x in xlist]
+    plt.bar(xlist, ylist, 100)
+    plt.xticks(xlist)
+    plt.yticks(ylist)
 
-plt.subplot(212)
-ylist = [timer1(test_list[x/10000-1]) for x in xlist]
-plt.bar(xlist, ylist, 100)
-plt.xticks(xlist)
-plt.yticks(ylist)
-plt.show()
+    plt.subplot(212)
+    ylist = [timer_for_sorting_bubble(test_list[x/10000-1]) for x in xlist]
+    plt.bar(xlist, ylist, 100)
+    plt.xticks(xlist)
+    plt.yticks(ylist)
+    plt.show()
 
 
