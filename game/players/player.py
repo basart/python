e7ec844e6460830b037d8afe514edaec464ec0c0
basart <basov_av@tut.by>
2016-01-14 17:33:07 +0300

    test serialization and deserialization

--- game/players/player.py
+++ game/players/player.py
@@ -3,7 +3,7 @@ import game.money_player.money_player as mp
 
 
 class Player(mp.MoneyPlayer):
-    def __init__(self, nick, login, password):
+    def __init__(self, nick=None, login=None, password=None):
         mp.MoneyPlayer.__init__(self)
         self.nick = nick
         self.login = login
@@ -11,8 +11,11 @@ class Player(mp.MoneyPlayer):
 
     def as_dict(self):
         d = {
-            "type": self.__class__.__name__,
-            "login": self.login
+            'type': self.__class__.__name__,
+            'nick': self.nick,
+            'login': self.login,
+            'password': self.password,
+            'bag': {'gold': self.total_number_of_gold, 'silver': self.total_number_of_silver}
         }
         return d
 
@@ -21,16 +24,30 @@ class Player(mp.MoneyPlayer):
 
     def load(self, file_object):
         object_as_dict = json.load(file_object)
-        self.login = object_as_dict["login"]
-        self.password = object_as_dict["password"]
+        self.nick = object_as_dict['nick']
+        self.login = object_as_dict['login']
+        self.password = object_as_dict['password']
+        self.total_number_of_silver = object_as_dict['bag']['silver']
+        self.total_number_of_gold = object_as_dict['bag']['gold']
         return object_as_dict
 
     def __str__(self):
-        return '{}(login="{}")'.format(self.__class__.__name__, self.login)
+        return '{}(nick="{}", login="{}", password="{}", gold={}, silver={})'.format(
+            self.__class__.__name__,
+            self.nick,
+            self.login,
+            self.password,
+            self.total_number_of_gold,
+            self.total_number_of_silver
+        )
 
 if __name__ == "__main__":
-    player1 = Player("vasgen", "vasya@mail.ru", "1111")
-    print(player1.__str__())
-    print(player1.total_number_of_gold)
-    player1.silver_to_gold(100)
-    print(player1.total_number_of_gold)
+    # player1 = Player("vasgen", "vasya@mail.ru", "1111")
+    # print(player1.__str__())
+    # print(player1.total_number_of_gold)
+    # player1.silver_to_gold(100)
+    # player1.save(open('object_'+player1.login+'.txt', 'w'))
+    # print(player1.total_number_of_gold)
+    deserialized_player = Player()
+    deserialized_player.load(open("object_vasya@mail.ru.txt"))
+    print("deserialized player is {}".format(deserialized_player))
