7b89c8d8cd07013a2a9271c95717c503dd29387f
basart <basov_av@tut.by>
2016-02-01 02:06:12 +0300

    corrected except and added argv[1]

--- lesson5/lesson5_home/image_processing.py
+++ lesson5/lesson5_home/image_processing.py
@@ -1,18 +1,21 @@
 import os
 from PIL import Image
+import sys
 
-def resize_image(path_image,dir, filename):
+
+def resize_image(path_image, dir, filename):
     try:
         img = Image.open(path_image)
         wigth, height = img.size
         new_img = img.resize((wigth/2, height/2), Image.ANTIALIAS)
         new_img.save(dir+'/resized_' + filename)
-
-    except:
-        pass
+        print("{} edited".format(filename))
+    except IOError:
+        print("{} isn't an image".format(filename))
 if __name__ == "__main__":
-    tree = os.walk(os.getcwd())
+    path_dir = sys.argv[1]
+    tree = os.walk(path_dir)
     for dirpath, dirname, filename in tree:
         for name in filename:
             resize_image(os.path.join(dirpath, name), dirpath, name)
-        #print(dirpath, dirname, filename)
+
